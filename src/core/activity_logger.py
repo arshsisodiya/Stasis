5d56612cedc6dcc863bde367ae55e1b089c5fa11
arshsisodiya <arshsisodiya@gmail.com>
2026-03-10 17:52:27 +0530

    fix: add missing activity_logger file omitted in c6153b9

--- src/core/activity_logger.py
+++ src/core/activity_logger.py
@@ -9,6 +9,7 @@ from pynput import mouse, keyboard
 from src.core.url_sniffer import get_browser_url
 from src.analytics.daily_summary import update_daily_stats
 from src.database.database import get_connection, get_setting
+from src.core.settings_cache import settings_cache
 from src.core.process_cache import process_cache
 from src.core.shutdown import shutdown_event
 import win32con
@@ -391,7 +392,7 @@ def get_active_window_info() -> dict | None:
         if not app_name:
             return None
 
-        browser_tracking = get_setting("browser_tracking", "1") == "1"
+        browser_tracking = settings_cache.get("browser_tracking", "1") == "1"
 
         url = "N/A"
 
@@ -632,7 +633,7 @@ def start_logging():
             info = get_active_window_info()
 
             # ---- determine idle state ----
-            idle_detection_enabled = get_setting("idle_detection", "1") == "1"
+            idle_detection_enabled = settings_cache.get("idle_detection", "1") == "1"
             idle_secs = input_tracker.get_idle_seconds() if idle_detection_enabled else 0
             media_playing = is_media_active(info)
             
